ed94b7485a63b066af222921dbcb186b504d70f0
Keshav <malaxpriya@gmail.com>
2026-09-13 09:53:06 +0530

    Create 152. Maximum Product Subarray.py

---
+++ Kadane/152. Maximum Product Subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        maxend = nums[0]
+        minend = nums[0]
+        res =    nums[0]
+
+        for i in range(1,len(nums)):
+            v1=nums[i]
+            v2=minend*nums[i]
+            v3=maxend*nums[i]
+
+            maxend=max(v1,max(v2,v3))
+            minend=min(v1,min(v2,v3))
+
+            res = max(res,max(minend,maxend))
+        
+        return res
